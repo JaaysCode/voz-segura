383a5d29560e13e8af64a8ef9db39608ac4f3498
Juan <jpcardonab0520@gmail.com>
2025-04-28 13:25:31 -0500

    chore: update components and pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Avatar from "./components/Avatar";
 import Navbar from "./components/navbar/Navbar";
+import Button from "./components/Button";
+
 export default function Home() {
   return (
     <div>
       <Avatar src={null} />
       <Navbar />
+      <Button/>
     </div>
   );
 }
---
+++ app/sign-up/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      LOGIN PAGE
+    </div>
+  )
+}
+
+export default page

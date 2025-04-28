1ecaef0398a4ebdaad1832932b6de834fe47a491
Pablo <jpcardonab0520@gmail.com>
2025-04-27 19:05:30 -0500

    Prueba de Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Avatar from "./components/Avatar";
-
+import Navbar from "./components/navbar/Navbar";
 export default function Home() {
   return (
     <div>
       <Avatar src={null} />
+      <Navbar />
     </div>
   );
 }
